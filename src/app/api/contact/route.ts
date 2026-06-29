c090fa8c514763db0bbfbd2514dd8c8bcfc62c20
Oluwaseun Deborah Adebayo <oluwaseuna294@gmail.com>
2026-06-29 16:50:14 +0100

    upd

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,9 +1,9 @@
-import { NextResponse, type NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 export async function POST(req: NextRequest) {
+  const resend = new Resend(process.env.RESEND_API_KEY); // moved inside
+
   const data = await req.json();
 
   await resend.emails.send({
